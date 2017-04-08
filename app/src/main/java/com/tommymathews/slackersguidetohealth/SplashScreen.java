e56cfbc3fbf17b7dc8652591094fd7325c30092b
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-08 18:00:15 -0400

    more stuff

---
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/SplashScreen.java
@@ -0,0 +1,30 @@
+package com.tommymathews.slackersguidetohealth;
+
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread myThread = new Thread(){
+            @Override
+            public void run(){
+                try {
+                    sleep(3000);
+                    Intent newIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(newIntent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        };
+        myThread.start();
+    }
+}
