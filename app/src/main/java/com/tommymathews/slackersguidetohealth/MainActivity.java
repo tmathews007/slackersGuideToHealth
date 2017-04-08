b9d612bd7dc6076a9e85bcfc50a218fdd1e7b1a6
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-08 19:13:26 -0400

    added in the page buttons to activity_main.xml

--- app/src/main/java/com/tommymathews/slackersguidetohealth/MainActivity.java
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/MainActivity.java
@@ -1,5 +1,8 @@
 package com.tommymathews.slackersguidetohealth;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 
 /**
@@ -7,4 +10,11 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class MainActivity extends AppCompatActivity{
+    private BottomNavigationView tabs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
 }
