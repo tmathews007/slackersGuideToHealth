b24c9290be338f51f3ac058284e6ca1b1eabdb02
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-08 18:32:27 -0400

    signup, login, and main pages made

--- app/src/main/java/com/tommymathews/slackersguidetohealth/MainActivity.java
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/MainActivity.java
@@ -1,8 +1,10 @@
 package com.tommymathews.slackersguidetohealth;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by Ashwin on 4/8/2017.
  */
 
-public class MainActivity {
+public class MainActivity extends AppCompatActivity{
 }
