d50d161330e782f2b67488e14949f9caded5aa9a
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-13 00:53:33 -0400

    made the ExerciseManager class

--- app/src/main/java/com/tommymathews/slackersguidetohealth/ExerciseManager.java
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/ExerciseManager.java
@@ -1,8 +1,17 @@
 package com.tommymathews.slackersguidetohealth;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
 /**
  * Created by Ashwin on 4/12/2017.
  */
 
-public class ExerciseManager {
+public class ExerciseManager extends Activity {
+
 }
