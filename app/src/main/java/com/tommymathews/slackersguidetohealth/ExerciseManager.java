5d8f8d9bf084fe6d76fe162fb64b910366a4b3a4
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-14 14:11:50 -0400

    commented in the skeleton for ExerciseManager and made slight changes to FoodManager

--- app/src/main/java/com/tommymathews/slackersguidetohealth/ExerciseManager.java
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/ExerciseManager.java
@@ -1,8 +1,30 @@
 package com.tommymathews.slackersguidetohealth;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Ashwin on 4/12/2017.
  */
 
-public class ExerciseManager {
+public class ExerciseManager extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_exercise_manager);
+
+        //Tommy and Greg will complete the if statements below.
+        //Depending on what part of the drawable that the user
+        //clicks on, this class will create a new fragment
+        //for the selected fitness group.
+
+
+        //button/etc areas here;
+        //if user clicks on abs, setOnClickListener{abs fragment}
+        //else if user clicks on back, setOnClickListener{back fragment}
+        //else if user clicks on biceps, setOnClickListener{biceps fragment}
+        //...
+        //else setOnClickListener{triceps fragment}
+    }
 }
