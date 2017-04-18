88328c553f4665d3f4863ade999d0351abd218c8
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-18 17:23:44 -0400

    connected ChestFragment with ChestActivity and ExerciseManager

--- app/src/main/java/com/tommymathews/slackersguidetohealth/ChestFragment.java
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/ChestFragment.java
@@ -1,12 +1,20 @@
 package com.tommymathews.slackersguidetohealth;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+
+import java.lang.reflect.Field;
 
 public class ChestFragment extends Fragment {
 
@@ -28,6 +36,36 @@ public class ChestFragment extends Fragment {
         // values package.
         chestWorkout = ( TextView ) view.findViewById( R.id.chest_workout );
 
+
+        Button chestWorkouts = (Button) view.findViewById(R.id.chestWorkoutButton);
+        chestWorkouts.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), ChestActivity.class);
+                startActivity(i);
+            }
+        });
+
         return view;
+    }
+
+    static void removeShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field");
+        } catch (IllegalAccessException e) {
+            Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode");
+        }
     }
 }
