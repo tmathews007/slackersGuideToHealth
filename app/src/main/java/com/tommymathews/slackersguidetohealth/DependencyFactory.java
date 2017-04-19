a9658e1087de6f5b8c48a7eb09734b6c943c0dbe
Christine <cyjean27@gmail.com>
2017-04-19 19:55:29 -0400

    updated dependency factory

--- app/src/main/java/com/tommymathews/slackersguidetohealth/DependencyFactory.java
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/DependencyFactory.java
@@ -1,8 +1,22 @@
 package com.tommymathews.slackersguidetohealth;
 
+import android.content.Context;
+
+import com.tommymathews.slackersguidetohealth.service.UserService;
+import com.tommymathews.slackersguidetohealth.service.impl.SQLiteUserService;
+
 /**
  * Created by cj on 4/19/17.
  */
 
 public class DependencyFactory {
+    private static UserService userService;
+
+    public static UserService getUserService(Context context) {
+        if (userService == null) {
+            //storyService = new InMemoryStoryService(context);
+            userService = new SQLiteUserService(context);
+        }
+        return userService;
+    }
 }
