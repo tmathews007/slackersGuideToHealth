c99746cc4136e41ccfd8f0eb887c721f82089ddb
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-19 21:17:06 -0400

    added in explore and fixed main activity button display

---
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/Cost.java
@@ -0,0 +1,17 @@
+package com.tommymathews.slackersguidetohealth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lidya on 4/6/2017.
+ */
+
+public class Cost extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cost);
+
+    }
+}
---
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/Distance.java
@@ -0,0 +1,18 @@
+package com.tommymathews.slackersguidetohealth;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lidya on 4/6/2017.
+ */
+
+public class Distance extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_distance);
+
+    }
+}
