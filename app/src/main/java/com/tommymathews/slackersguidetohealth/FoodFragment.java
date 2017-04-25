39b858dac3151dfb1f67511cdc9c920debc44ae2
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-25 17:42:58 -0400

    FoodFragment changed slightly

--- app/src/main/java/com/tommymathews/slackersguidetohealth/FoodFragment.java
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/FoodFragment.java
@@ -87,12 +87,12 @@ public class FoodFragment extends Fragment{
                     getActivity().overridePendingTransition(0,0);
                 } else if (item.getItemId() == R.id.foodItem) {
                 } else if (item.getItemId() == R.id.exploreItem) {
-                    //Intent i = new Intent(getActivity(), ExploreMain.class);
-                    //startActivity(i);
+                    Intent i = new Intent(getActivity(), ExploreMain.class);
+                    startActivity(i);
                     getActivity().overridePendingTransition(0,0);
                 } else if (item.getItemId() == R.id.funItem) {
-                    //Intent intent = new Intent(getActivity(), Events.class);
-                    //startActivity(intent);
+                    Intent intent = new Intent(getActivity(), Events.class);
+                    startActivity(intent);
                     getActivity().overridePendingTransition(0,0);
                 }
                 return false;
