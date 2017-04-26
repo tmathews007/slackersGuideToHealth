f16b949a3a2a423a1bf17314722d2f8691f9a48d
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-26 18:25:45 -0400

    fixed the navigation for the pages and made the purchases page

---
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/PurchasesActivity.java
@@ -0,0 +1,10 @@
+package com.tommymathews.slackersguidetohealth;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ashwin on 4/26/2017.
+ */
+
+public class PurchasesActivity extends AppCompatActivity {
+}
