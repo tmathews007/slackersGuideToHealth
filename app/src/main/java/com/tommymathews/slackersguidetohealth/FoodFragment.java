d47d4e199121ca67055d42638fb9d145e5e2c712
Ashwin Kumar <realashwinkumar@gmail.com>
2017-04-26 20:59:02 -0400

    Merge branch 'master' of C:\Users\Ashwin\AndroidStudioProjects\slackersGuideToHealth with conflicts.

--- app/src/main/java/com/tommymathews/slackersguidetohealth/FoodFragment.java
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/FoodFragment.java
@@ -3,11 +3,13 @@ package com.tommymathews.slackersguidetohealth;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -23,10 +25,7 @@ public class FoodFragment extends Fragment{
     //TODO get the activity here and redirect to the page
     // with the suggestions and recipe
 
-    public static final String CALORIES = "CALORIES";
-
-    private Button nextPageButton;
-    private EditText calories;
+    public final String CALORIES = "CALORIES";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,42 +36,42 @@ public class FoodFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.activity_food, container, false);
 
-        nextPageButton = (Button) view.findViewById(R.id.nextPageFood);
-        calories = (EditText) view.findViewById(R.id.calories);
+        Button nextPageButton = (Button) view.findViewById(R.id.nextPageFood);
 
         nextPageButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                 String cal = calories.getText().toString();
+                EditText calories = (EditText) view.findViewById(R.id.calories);
+                String cal = calories.getText().toString();
                 int numCal = Integer.parseInt(cal);
-                    Intent i = new Intent(getActivity(), FoodActivity.class);
+//                if(numCal<500){
+                    //TODO Ashwin/Christine week of 4/18
+                    Intent i = new Intent(getActivity(), FoodActivity2.class);
                     i.putExtra(CALORIES, numCal);
                     startActivity(i);
-
+//                }
+//                else if(numCal>=500 && numCal<750) {
+//                    Intent i = new Intent(getActivity(), FoodActivity.class);
+//                    startActivity(i);
+//                }
+//                else if(numCal>=750 && numCal<1000){
+//                    //TODO Ashwin/Christine week of 4/18
+//                    Intent i = new Intent(getActivity(), FoodActivity3.class);
+//                    startActivity(i);
+//                }
+//                else if(numCal>=1000 && numCal<=1500){
+//                    //TODO Ashwin/Christine week of 4/18
+//                    Intent i = new Intent(getActivity(), FoodActivity4.class);
+//                    startActivity(i);
+//                }
+//                else{
+//                    Toast.makeText(getActivity().getApplicationContext(), "Talk to a professional" +
+//                            " to develop a diet plan. Your calorie intake is too high.", Toast.LENGTH_SHORT).show();
+//                }
             }
         });
 
         return view;
 
     }
-    static void removeShiftMode(BottomNavigationView view) {
-        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
-        try {
-            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
-            shiftingMode.setAccessible(true);
-            shiftingMode.setBoolean(menuView, false);
-            shiftingMode.setAccessible(false);
-            for (int i = 0; i < menuView.getChildCount(); i++) {
-                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
-                item.setShiftingMode(false);
-                // set once again checked value, so view will be updated
-                item.setChecked(item.getItemData().isChecked());
-            }
-        } catch (NoSuchFieldException e) {
-            Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field");
-        } catch (IllegalAccessException e) {
-            Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode");
-        }
-    }
-
 }
