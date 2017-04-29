185eb304c3e4b58ae8f6205b8a4ca7f4f585a170
Ashley <ashleypcheng@gmail.com>
2017-04-29 19:01:11 -0400

    Git didn't commit everything

---
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/Settings.java
@@ -0,0 +1,280 @@
+package com.tommymathews.slackersguidetohealth;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tommymathews.slackersguidetohealth.model.User;
+import com.tommymathews.slackersguidetohealth.service.UserService;
+import com.tommymathews.slackersguidetohealth.service.impl.DbSchema;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Ashley on 4/29/17.
+ */
+
+public class Settings extends Activity {
+
+    public static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+            Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+
+    // UI references.
+    private EditText nameEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
+    private RadioGroup genderRadioGroup;
+    private SeekBar ageSeekBar;
+    private TextView ageTextView;
+    private SeekBar weightSeekBar;
+    private TextView weightTextView;
+    private SeekBar heightSeekBar;
+    private TextView heightTextView;
+    private Spinner goalSpinner;
+    private UserService userService;
+    private Button saveSettingsButton;
+    private int age;
+    private int weight;
+    private int height;
+
+    private SharedPreferences sharedPreferences;
+    private String email;
+    private User user;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        sharedPreferences=getSharedPreferences(DbSchema.LOGIN, 0);
+
+        userService = DependencyFactory.getUserService(getApplicationContext());
+        email = sharedPreferences.getString(DbSchema.EMAIL,null);
+        user = userService.getUserByEmail(email);
+
+        nameEditText = (EditText) findViewById(R.id.name);
+        nameEditText.setText(user.getName());
+
+        genderRadioGroup = (RadioGroup) findViewById(R.id.gender_radio_group);
+        if(user.getGender() == User.Gender.MALE) {
+            genderRadioGroup.check(R.id.male_button);
+        } else {
+            genderRadioGroup.check(R.id.female_button);
+        }
+
+        // Set up the login form.
+        emailEditText = (EditText) findViewById(R.id.email);
+        emailEditText.setText(user.getEmail());
+        emailEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!isEmailValid(emailEditText.getText().toString()))
+                    emailEditText.setTextColor(Color.RED);
+                else
+                    emailEditText.setTextColor(Color.BLACK);
+            }
+        });
+
+        passwordEditText = (EditText) findViewById(R.id.pass_one);
+        passwordEditText.setText(user.getPassword());
+        confirmPasswordEditText = (EditText) findViewById(R.id.pass_confirm);
+        confirmPasswordEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (!confirmPasswordEditText.getText().toString().equals(passwordEditText.getText().toString()))
+                    Toast.makeText(getApplicationContext(), "Password must match", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ageTextView = (TextView) findViewById(R.id.txt_age);
+        ageSeekBar = (SeekBar) findViewById(R.id.seekbar_age);
+        ageSeekBar.setProgress(user.getAge());
+        ageTextView.setText(Integer.toString(user.getAge()));
+        ageSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                ageTextView.setText((i + user.getAge()) + "");
+                age = i + user.getAge();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        weightTextView = (TextView) findViewById(R.id.weight_display_text);
+        weightTextView.setText(Integer.toString(user.getWeight())+ "lbs.");
+        weightSeekBar = (SeekBar) findViewById(R.id.weight_bar);
+        weightSeekBar.setProgress(user.getWeight());
+        weightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                weightTextView.setText((i + user.getWeight()) + "lbs.");
+                weight = i + user.getWeight();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        heightTextView = (TextView) findViewById(R.id.txt_height);
+        heightSeekBar = (SeekBar) findViewById(R.id.seekbar_height);
+        heightSeekBar.setProgress(user.getHeight());
+        heightTextView.setText(user.convertHeight(user.getHeight()));
+        heightSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                heightTextView.setText( (i / 12) + "' " + (i % 12) + "\"");
+                height = i;
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        goalSpinner = (Spinner) findViewById(R.id.goal_spinner);
+        ArrayAdapter<CharSequence> goalAdapter = ArrayAdapter.createFromResource(this, R.array.goals_array, android.R.layout.simple_spinner_item);
+        goalAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        goalSpinner.setAdapter(goalAdapter); // still need to fill this out
+
+        saveSettingsButton = (Button) findViewById(R.id.btn_save_settings);
+        saveSettingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!attemptSave()) {
+
+                    SharedPreferences.Editor editor=sharedPreferences.edit();
+                    editor.putString(DbSchema.EMAIL, emailEditText.getText().toString());
+                    editor.commit();
+
+                    user.setName(nameEditText.getText().toString());
+                    user.setEmail(emailEditText.getText().toString());
+                    user.setPassword(passwordEditText.getText().toString());
+                    switch (genderRadioGroup.getCheckedRadioButtonId()) {
+                        case (R.id.female_button):
+                            user.setGender(User.Gender.FEMALE);
+                            break;
+                        default:
+                            user.setGender(User.Gender.MALE);
+                    }
+                    user.setAge(age);
+                    user.setHeight(height);
+                    user.setWeight(weight);
+                    user.setFitnessGoal(goalSpinner.getSelectedItemPosition());
+
+//                    UserService userService = DependencyFactory.getUserService(getApplication());
+                    userService.addUser(user);
+
+                    startActivity(new Intent(Settings.this, ProfileActivity.class));
+                    finish();
+                    //store everything in shared preferences for now
+                }
+            }
+        });
+    }
+
+
+    /**
+     * Attempts to sign in or register the account specified by the login form.
+     * If there are form errors (invalid email, missing fields, etc.), the
+     * errors are presented and no actual login attempt is made.
+     */
+    private boolean attemptSave() {
+        // Store values at the time of the save attempt.
+
+        String name = nameEditText.getText().toString();
+        String email = emailEditText.getText().toString();
+        String password = passwordEditText.getText().toString();
+        boolean cancel = false;
+
+        // Check for a valid password, if the user entered one.
+        if (TextUtils.isEmpty(password)) {
+            passwordEditText.setHint("Password required");
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            confirmPasswordEditText.setHint("Confirm Password required");
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            emailEditText.setHint("Email required");
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            cancel = true;
+        }
+
+        if (TextUtils.isEmpty(name)) {
+            nameEditText.setHint("Name required");
+            cancel = true;
+        }
+
+        return cancel;
+    }
+
+    public static boolean isEmailValid(String emailStr) {
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX .matcher(emailStr);
+        return matcher.find();
+    }
+
+}
+
+
---
+++ slackersGuideToHealth/app/src/main/java/com/tommymathews/slackersguidetohealth/FoodFragment.java
@@ -0,0 +1,124 @@
+package com.tommymathews.slackersguidetohealth;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationItemView;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.lang.reflect.Field;
+
+/**
+ * Created by Ashwin on 4/4/2017.
+ */
+
+public class FoodFragment extends Fragment{
+    //TODO get the activity here and redirect to the page
+    // with the suggestions and recipe
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.activity_food, container, false);
+
+        Button nextPageButton = (Button) view.findViewById(R.id.nextPageFood);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) view.findViewById(R.id.bottomNavigationView);
+        removeShiftMode(bottomNavigationView);
+        bottomNavigationView.getMenu().getItem(0).setChecked(false);
+        bottomNavigationView.getMenu().getItem(1).setChecked(true);
+        bottomNavigationView.getMenu().getItem(2).setChecked(false);
+        bottomNavigationView.getMenu().getItem(3).setChecked(false);
+
+
+        nextPageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                //to fix
+                EditText calories = (EditText) view.findViewById(R.id.calories);
+                String cal = calories.getText().toString();
+                int numCal = Integer.parseInt(cal);
+                if(numCal<500){
+                    Intent i = new Intent(getActivity(), FoodActivity.class);
+                    startActivity(i);
+                }
+                else if(numCal>=500 && numCal<750) {
+                    Intent i = new Intent(getActivity(), FoodActivity.class);
+                    startActivity(i);
+                }
+                else if(numCal>=750 && numCal<1000){
+                    Intent i = new Intent(getActivity(), FoodActivity.class);
+                    startActivity(i);
+                }
+                else if(numCal>=1000 && numCal<=1500){
+                    Intent i = new Intent(getActivity(), FoodActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(getActivity().getApplicationContext(), "Talk to a professional" +
+                            " to develop a diet plan. Your calorie intake is too high.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        /* Use this to switch between tabs. */
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.exerciseItem) {
+                    Intent i = new Intent(getActivity(), ExerciseManager.class);
+                    startActivity(i);
+                    getActivity().overridePendingTransition(0,0);
+                } else if (item.getItemId() == R.id.foodItem) {
+                } else if (item.getItemId() == R.id.exploreItem) {
+                    //Intent i = new Intent(getActivity(), ExploreMain.class);
+                    //startActivity(i);
+                    getActivity().overridePendingTransition(0,0);
+                } else if (item.getItemId() == R.id.funItem) {
+                    //Intent intent = new Intent(getActivity(), Events.class);
+                    //startActivity(intent);
+                    getActivity().overridePendingTransition(0,0);
+                }
+                return false;
+            }
+        });
+
+        return view;
+
+    }
+    static void removeShiftMode(BottomNavigationView view) {
+        BottomNavigationMenuView menuView = (BottomNavigationMenuView) view.getChildAt(0);
+        try {
+            Field shiftingMode = menuView.getClass().getDeclaredField("mShiftingMode");
+            shiftingMode.setAccessible(true);
+            shiftingMode.setBoolean(menuView, false);
+            shiftingMode.setAccessible(false);
+            for (int i = 0; i < menuView.getChildCount(); i++) {
+                BottomNavigationItemView item = (BottomNavigationItemView) menuView.getChildAt(i);
+                item.setShiftingMode(false);
+                // set once again checked value, so view will be updated
+                item.setChecked(item.getItemData().isChecked());
+            }
+        } catch (NoSuchFieldException e) {
+            Log.e("ERROR NO SUCH FIELD", "Unable to get shift mode field");
+        } catch (IllegalAccessException e) {
+            Log.e("ERROR ILLEGAL ALG", "Unable to change value of shift mode");
+        }
+    }
+
+}
