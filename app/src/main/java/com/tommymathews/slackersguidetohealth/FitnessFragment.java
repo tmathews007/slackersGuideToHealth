1531a7c661946f294a11a16693529cc90af02e5b
Thomas Mathews <tmathews@terpmail.umd.edu>
2017-05-08 16:12:45 -0400

    Updated gradle file to reflect Tommy's Android version. Just a test to pull the changes that have been made.

--- app/src/main/java/com/tommymathews/slackersguidetohealth/FitnessFragment.java
+++ app/src/main/java/com/tommymathews/slackersguidetohealth/FitnessFragment.java
@@ -1,12 +1,16 @@
 package com.tommymathews.slackersguidetohealth;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,13 +23,16 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.tommymathews.slackersguidetohealth.model.Fitness;
+import com.tommymathews.slackersguidetohealth.model.Food;
 import com.tommymathews.slackersguidetohealth.model.Playlist;
 import com.tommymathews.slackersguidetohealth.service.FitnessService;
+import com.tommymathews.slackersguidetohealth.service.FoodService;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class FitnessFragment extends Fragment {
+    private static final String FITNESS = "FITNESS";
     private final String EXTRA_PLAYLIST_CREATED = "EXTRA_PLAYLIST_CREATED";
 
     private final int MATCH_DISTANCE = 10;
@@ -39,6 +46,10 @@ public class FitnessFragment extends Fragment {
     private Button createWorkoutButton;
     private Button backViewButton;
 
+    private ProgressDialog dialog;
+
+    String name = "";
+
     public static Fragment newInstance() {
         FitnessFragment fragment = new FitnessFragment();
 
@@ -50,6 +61,8 @@ public class FitnessFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fitness_fragment, container, false);
+
+
 
         //random
         view.setOnTouchListener(new View.OnTouchListener() {
@@ -68,6 +81,8 @@ public class FitnessFragment extends Fragment {
                         Playlist playlist = new Playlist("Ab Workouts" ,img, fitnesses);
                         DependencyFactory.getPlaylistService(getActivity()).addPlaylist(playlist);
 
+                        name = "ABS";
+
                         Intent intent = new Intent(getActivity(), FitnessDescriptionActivity.class);
                         intent.putExtra("ID", playlist.getId());
                         startActivity(intent);
@@ -76,19 +91,26 @@ public class FitnessFragment extends Fragment {
 
                     case BICEPS:
                         Log.d("Clicked", "BICEPS");
+                        name = "BICEPS";
                         startActivity(new Intent(getActivity(), FitnessDescriptionActivity.class));
                         break;
 
                     case CHEST:
                         Log.d("Clicked", "CHEST");
+                        name = "CHEST";
                         startActivity(new Intent(getActivity(), FitnessDescriptionActivity.class));
                         break;
 
                     case QUADS:
                         Log.d("Clicked", "QUADS");
+                        name = "QUADS";
                         startActivity(new Intent(getActivity(), FitnessDescriptionActivity.class));
                         break;
                 }
+
+//                if( ! name.equals( "" ) )
+//                    new ReadFromFitnessDB().execute( name );
+
                 return true;
             }
         });
@@ -129,9 +151,10 @@ public class FitnessFragment extends Fragment {
 
                 DependencyFactory.getFitnessService(getActivity()).addFitness(test);
 
-
                 intent.putExtra("ID", test.getId());
                 startActivity( intent );
+
+
             }
         }
         );
@@ -163,6 +186,69 @@ public class FitnessFragment extends Fragment {
         return img.getPixel(x, y);
 
     }
+
+//    private void passInFitness(List<Fitness> fitnesses) {
+//        if (fitnesses == null || fitnesses.isEmpty()) {
+//            new AlertDialog.Builder(getActivity())
+//                    .setTitle("No Workouts!").setMessage("We couldn't find a workout for the bodypart that you clicked. Do you want to try " +
+//                    "another workout?")
+//                    .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            Intent i = new Intent(getActivity(), FitnessBacklogActivity.class);
+//                            startActivity(i);
+//                            getActivity().finish();
+//                        }
+//                    })
+//                    .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+//                        @Override
+//                        public void onClick(DialogInterface dialog, int which) {
+//                            Intent i = new Intent(getActivity(), MainActivity.class);
+//                            startActivity(i);
+//                            getActivity().finish();
+//                        }
+//                    }).create().show();
+//        } else {
+//            Fitness fitness = fitnesses.get((int)(Math. random() * fitnesses.size()));
+//            Intent i = new Intent(getActivity(), FitnessBacklogActivity.class);
+//            i.putExtra(FITNESS, fitness);
+//            startActivity(i);
+//        }
+//    }
+//
+//    @Override
+//    public void onPause(){
+//
+//        super.onPause();
+//        if(dialog != null)
+//            dialog.dismiss();
+//    }
+//
+//    private class ReadFromFitnessDB extends AsyncTask<String, Void, List<Fitness>> {
+//
+//        private Fitness fitness;
+//
+//        protected void onPreExecute() {
+//            dialog = new ProgressDialog(getActivity());
+//            dialog.setMessage("Getting workouts");
+//            dialog.show();
+//        }
+//
+//        @Override
+//        protected List<Fitness> doInBackground(String... params) {
+//            FitnessService fitnessService = DependencyFactory.getFitnessService(getActivity());
+//            return fitnessService.getFitnessesByName( params[0] );
+//        }
+//
+//        @Override
+//        protected void onPostExecute(List<Fitness> fitnesses ) {
+//            super.onPostExecute(fitnesses);
+//            if (dialog.isShowing()) {
+//                dialog.dismiss();
+//            }
+//            passInFitness(fitnesses);
+//        }
+//    }
 
 
 }
